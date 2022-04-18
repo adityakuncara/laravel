72aa3bf49bef892fb14bf724e33146a46ee81073
adityakuncara <akun65552@gmail.com>
2022-04-19 01:13:40 +0700

    praktikum 1 jobsheet 1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+/*
+------------------------------------------------
+*/
+
+route:: get('/',function() {
+    return 'selamat datang';
+});
+
+route:: get('/about', function(){
+    echo 'Nama : Aditya Kuncara Bakti';
+    echo '<br>NIM : 2041720231';
+    echo '<br>Kelas : TI 2H';
+});
+
+route:: get('/articels/{id}',function($id){
+    return 'Ini adalah halaman artikel dengan ID: '.$id;
 });
